8edfb21cebbc809e181cd5d8735eb843aa9efbb4
Philip <pch4623@gmail.com>
2022-02-06 00:26:03 +0900

    Action creator 오타 수정

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 // Action Creator 정의
 
 // Action 1) 곡 선택하기
-export const selectSongs = (song) => {  // Named export : 하나의 파일에서 여러 함수 export 하는 경우 사용
+export const selectSong = (song) => {  // Named export : 하나의 파일에서 여러 함수 export 하는 경우 사용
   return {
     type: 'SONG_SELECTED',
     payload: song
