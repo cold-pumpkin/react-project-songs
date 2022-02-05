fd987f5a724ea9b37c771f0237ec52c8a2c84cf4
Philip <pch4623@gmail.com>
2022-02-05 11:13:18 +0900

    Reducer 정의
    1) 곡 리스트를 리턴하는 Reducer
    2) 선택된 곡을 리턴하는 Reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+// Reducers 정의
+
+// Reducer 1) 곡 리스트 리턴
+const songsReducer = () => {
+  return [
+    { title: '12:45(Stripped)', duration: '3:00' },
+    { title: 'abcdefu', duration: '2:49' },
+    { title: 'Levitating', duration: '3:24' },
+    { title: 'Levitating', duration: '3:24' },
+    { title: 'Hush Hush', duration: '3:04' },
+  ];
+};
+
+// Reducer 2) 선택된 곡 리턴
+const selectedSongReducer = (selectedSong = null, action) => {  // 기존에 선택된 곡은 default 값을 null 셋팅
+  if (action === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+}
