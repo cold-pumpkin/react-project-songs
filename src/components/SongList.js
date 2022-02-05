7482583f6035d0423d04658a0a070f4ef590e860
Philip <pch4623@gmail.com>
2022-02-06 00:29:26 +0900

    action 연동 되도록 connect 파라미터에 추가

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   // mapStateToProps에서 리턴한 this.props 가져와 렌더링
@@ -8,7 +9,11 @@ class SongList extends Component {
       return (
         <div className='item' key={song.title}>
           <div className='right floated content'>
-            <button className='ui button primary'>
+            <button 
+              className='ui button primary'
+              onClick={() => {this.props.selectSong(song)}} 
+              // 클릭 시 Action 동작
+            >
               Select
             </button>
           </div>
@@ -24,8 +29,11 @@ class SongList extends Component {
 }
 
 // Redux Store의 모든 state 정보 가져오기 : connect(state 가져올 함수)(현재 컴포넌트)
+// Redux Store 정보 바뀌면 재실행 됨
 const mapStateToProps = (state) => {
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+// connect(state 가져올 함수, { action creator })(현재 컴포넌트)
+// action creator Object key-value 같은 경우 하나로 생각 가능 (ES6)
+export default connect(mapStateToProps, { selectSong })(SongList);  
