d34fb1c15658f7381a1a07c56229e5a67b0047c7
Philip <pch4623@gmail.com>
2022-02-05 11:03:08 +0900

    Action Creator 디렉토리 생성 및 노래 선택 Action 생성

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+// Action Creator 정의
+
+// Action 1) 곡 선택하기
+export const selectSongs = (song) => {  // Named export : 하나의 파일에서 여러 함수 export 하는 경우 사용
+  return {
+    type: 'SONG_SELECTED',
+    payload: song
+  };
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { selectSongs } from '../actions';  // named export 인 경우 {...} & index.js 생략 가능
 
 const App = () => {
   return <div>App!!</div>
-}
-;
+};
 
 export default App;
