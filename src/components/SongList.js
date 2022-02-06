3837515ddffbd42da6defb68f1fb6cb99e6fb18c
Philip <pch4623@gmail.com>
2022-02-06 11:01:50 +0900

    주석 수정

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -29,11 +29,14 @@ class SongList extends Component {
 }
 
 // Redux Store의 모든 state 정보 가져오기 : connect(state 가져올 함수)(현재 컴포넌트)
-// Redux Store 정보 바뀌면 재실행 됨
+// 렌더링 or Redux Store 정보 바뀌면 재실행 됨
 const mapStateToProps = (state) => {
   return { songs: state.songs };
 }
 
 // connect(state 가져올 함수, { action creator })(현재 컴포넌트)
 // action creator Object key-value 같은 경우 하나로 생각 가능 (ES6)
-export default connect(mapStateToProps, { selectSong })(SongList);  
+export default connect(
+  mapStateToProps, 
+  { selectSong }  // -> props.selectSong() 호출할 때마다 dispatch 해줌
+)(SongList);  
